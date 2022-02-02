18588f33e2013881c3091ea2f9c17011a3f17ac4
Yazid <yalhujery01@gmail.com>
2022-02-01 22:01:41 -0500

    .

---
+++ assets/script.js
@@ -0,0 +1,9 @@
+var startBtn = document.getElementById("startBtn")
+
+startBtn.addEventListener("click", clearPg)
+
+function clearPg (){
+
+   document.getElementById("startPg").style.display = "none"
+
+}
