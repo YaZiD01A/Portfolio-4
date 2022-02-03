b75fa117aae61399b5f5bf8a306a5793d3b57c56
Yazid <yalhujery01@gmail.com>
2022-02-02 21:18:59 -0500

    .

--- assets/script.js
+++ assets/script.js
@@ -1,9 +1,27 @@
 var startBtn = document.getElementById("startBtn")
+var question = document.getElementById("question")
+
 
 startBtn.addEventListener("click", clearPg)
 
 function clearPg (){
 
    document.getElementById("startPg").style.display = "none"
+
+}
+
+document.getElementById("qPg").style.display = "none"
+
+if(startBtn.addEventListener("click", clearPg)){
+
+   document.getElementById("qPg").style.display = "!none"
+
+}
+
+question.addEventListener("click", clearPgQ)
+
+function clearPgQ (){
+
+   document.getElementById("question").style.display = "none"
 
 }
